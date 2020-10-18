0ad19445e867f5dd448c377494c334c4c7f395ba
reizha fajrian <reizha77@gmail.com>
2020-10-18 16:01:15 +0700

    splash screen

---
+++ BusTicketActivity/app/src/main/java/com/example/busticketactivity/SignInAndRegistActivity.kt
@@ -0,0 +1,11 @@
+package com.example.busticketactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInAndRegistActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in_and_regist)
+    }
+}
